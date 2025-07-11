b187bed76904591d6a3e240c692cc73270fa0910
lminervino18 <lminervino@fi.uba.ar>
2025-07-11 04:49:36 -0300

    feat: add GSAP and OGL libraries, implement Particles component, and enhance AboutMe page with profile details and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AboutMe } from './pages/AboutMe'
 import { Projects } from './pages/Projects'
 import { NavBar } from './components/NavBar'
+import Particles from './components/Particles'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    <div className="app-wrapper">
+      <Particles
+        particleColors={['#ffffff']}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+      />
+      <div className="app-content">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   )
 }
 
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,9 +1,13 @@
-import { COLORS } from '../constants/colors'
+import './styles/AboutMe.css'
 
 export function AboutMe() {
   return (
-    <main> 
-      {/* About Me content will go here */}
+    <main className="about-container">
+      <div className="about-content">
+        <img src="/src/assets/me/logo.jpeg" alt="Profile" className="profile-img" />
+        <h1>Hi, I'm Lorenzo Minervino</h1>
+        <p>Software engineering student passionate about backend, systems design, and distributed architectures.</p>
+      </div>
     </main>
   )
 }
